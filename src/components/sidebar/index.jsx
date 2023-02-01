8958ae544dfc115214655c674e3f9c441439192d
LauricioX <developerwebmasterpro@gmail.com>
2023-02-01 09:54:40 -0300

    sidebar

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,23 +1,40 @@
 import { useState } from 'react';
-import { Button, Container } from './styles';
-import { FaArrowAltCircleLeft } from 'react-icons/fa';
+import { Button, Container, ContainerSideInfo, SideDown, SideUp } from './styles';
+import {  FaArrowCircleLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import lauricioPhoto from "../../assets/lauricio.png"
 
 function Sidebar(){
-  const [side, setSide] = useState(false);
+  const [side, setSide] = useState(true);
   
   return(
     <>
      <Container 
     width={side ? 200 : 0}
     visibility={side ? "visible" : "hidden"}>
-      
+     <ContainerSideInfo>
+      <SideUp display={side ? "flex" :"none"}>
+         <img src={lauricioPhoto} alt="" />
+         <h4>Laurício De Souza</h4>
+         <p>FullStack Developer</p>
+      </SideUp>
+      <SideDown display={side ? "flex" :"none"}>
+        <Link to="/" >Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/technologies">Technologies</Link>
+        <Link to="/contact">Contact</Link>
+      </SideDown>
+
+     </ContainerSideInfo>
     </Container>
      <Button onClick={() => setSide(!side)} 
      rot={side ? 0 : 180}
-     posi={side ? 40 : 150}
+     posi={side ? 140 : 10}
      >
-       <FaArrowAltCircleLeft/>
-    </Button>      
+       <FaArrowCircleLeft/>
+    </Button>  
+
+   
     </>
    
   )
--- src/components/sidebar/styles.js
+++ src/components/sidebar/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
  export const Container = styled.div`
-  background-color: #344;
+  background-color: #7B838C;
   height: 100vh;
   width: 100%;
   transition: all .4s;
@@ -11,15 +11,81 @@ import styled from 'styled-components';
 
 
 export const Button = styled.button`
-background-color: #444;
+background-color: #4444;
 position: absolute;
 top: 10px;
-left: (${props => `${props.posi}px`});
-width: 50px;
-height: 30px;
-line-height: 30px;
+left:${props => `${props.posi}px`};
 color: white;
-font-size: 1.3rem;
+padding:5px;
 transition: all .4s;
 transform: rotate(${props => `${props.rot}deg`});
+border:none;
+
+& > svg{
+  font-size:1.2rem;
+}
+`
+
+export const ContainerSideInfo = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 90px;
+padding-top: 20px;
+`
+
+export  const SideUp = styled.div`
+height: 30%;
+padding: 30px;
+display: flex;
+flex-direction: column;
+gap: 10px;
+text-align: left;
+display: ${props => props.display};
+
+& > img{
+  width: 100%;
+}
+`
+export  const SideDown = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+gap: 10px;
+text-align: left;
+transition: all .4s;
+padding-left: 30px;
+display:${props => props.display};
+
+
+
+
+& > a{
+  font-size: 1.1rem;
+  text-decoration: none;
+  color: #000;
+  font-weight: 600;
+  transition: all .4s;
+}
+
+
+& > a::after{
+  content: "";
+  position: absolute;
+  background-color:#000;
+  left: 10px;
+  transition: all .4s;
+
+}
+
+& > a:hover::after{
+  background-color:#000;
+  width: 4px;
+  height: 20px;
+  color: #000;
+  transition: all .4s;
+
+} 
 `
